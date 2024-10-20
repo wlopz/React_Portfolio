fe918fd5dd773a0664f7c3d9863beff5d37388fd
Wilfredo <jrlopez85@gmail.com>
2024-10-19 22:36:13 -0500

    Adding logo to the displays

--- src/components/HaxRoom.jsx
+++ src/components/HaxRoom.jsx
@@ -10,7 +10,7 @@ const HaxRoom = (props) => {
   const { nodes, materials } = useGLTF('/models/hacker-room.glb');
 
   const monitortxt = useTexture('textures/desk/monitor.png');
-  const screenTxt = useTexture('textures/desk/screen.png');
+  const screenTxt = useTexture('textures/desk/screen_wl.png');
 
   return (
     <group {...props} dispose={null}>
